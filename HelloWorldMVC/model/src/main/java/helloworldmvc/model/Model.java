241082dbe6a2e4c262f8731e09ec9c908172516b
raph5 <raph5@169.254.235.14>
2018-03-19 15:15:08 +0100

    Correction de l'affichage du message.

--- HelloWorldMVC/model/src/main/java/helloworldmvc/model/Model.java
+++ HelloWorldMVC/model/src/main/java/helloworldmvc/model/Model.java
@@ -1,9 +1,34 @@
 package helloworldmvc.model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Model implements IModel{
 	
 	public String getMessage() {
-		return Model.class.getResource("message.txt").getFile();
+		File f = new File(Model.class.getResource("./message.txt").getFile());
+		FileReader fr;
+		String line;
+		String res = "";
+		try {
+			fr = new FileReader(f);
+			BufferedReader b = new BufferedReader(fr);
+			while((line = b.readLine()) != null) {
+                res += line;
+            } 
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return res;
+		
 	}
 
 }
